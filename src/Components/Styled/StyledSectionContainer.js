c2264499db57ebbbfe1e524ebd22a97d6de6b313
FJ-Riveros <fran.etnies@gmail.com>
2020-07-24 16:54:40 +0200

    Minor changes to the background

--- src/Components/Styled/StyledSectionContainer.js
+++ src/Components/Styled/StyledSectionContainer.js
@@ -5,10 +5,11 @@ export default styled.div`
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
-  width: 60%;
+  width: 45%;
   border-radius: 1.5rem;
   box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.2);
   margin: 40px 0 40px 0;
+  background-color: #ffff;
   min-height: ${(props) => (props.minheight ? "70vh" : "none")};
   table {
     border: 2px solid black;
