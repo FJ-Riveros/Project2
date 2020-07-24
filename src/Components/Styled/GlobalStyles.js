5aaa88ec06b4016a8d1568958fd695c9adfd4af9
FJ-Riveros <fran.etnies@gmail.com>
2020-07-24 16:27:36 +0200

    Styling some components

--- src/Components/Styled/GlobalStyles.js
+++ src/Components/Styled/GlobalStyles.js
@@ -7,6 +7,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
+    background-color: #f3f7f7;
   }
   `;
 
--- src/Components/Styled/StyledSectionContainer.js
+++ src/Components/Styled/StyledSectionContainer.js
@@ -5,6 +5,10 @@ export default styled.div`
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+  width: 60%;
+  border-radius: 1.5rem;
+  box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.2);
+  margin: 40px 0 40px 0;
   min-height: ${(props) => (props.minheight ? "70vh" : "none")};
   table {
     border: 2px solid black;
