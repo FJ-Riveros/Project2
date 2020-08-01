94670657891bb810fc4831f491c7b66c30a32bd7
FJ-Riveros <fran.etnies@gmail.com>
2020-08-01 12:12:08 +0200

    Get request succesfull

--- src/API/Routes/index.js
+++ src/API/Routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const usuarioController = require("../Controllers/UserController");
+const Post = require("../Models/User");
 
 module.exports = function () {
   //Agrega nuevos pacientes via POST
@@ -9,6 +10,7 @@ module.exports = function () {
   router.get("/getusuarios", async (req, res) => {
     try {
       const Posts = await Post.find();
+      res.json(Posts);
     } catch (error) {
       res.json({ message: error });
     }
