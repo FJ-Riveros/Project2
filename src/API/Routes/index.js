3e8e06c298d34506f322c5c577e817637401a89a
FJ-Riveros <fran.etnies@gmail.com>
2020-08-01 12:36:04 +0200

    The route Get for a single user works

--- src/API/Routes/index.js
+++ src/API/Routes/index.js
@@ -5,12 +5,29 @@ const Post = require("../Models/User");
 
 module.exports = function () {
   //Agrega nuevos pacientes via POST
-  router.post("/usuarios", usuarioController.nuevoUsuario);
+  router.post("/adduser", usuarioController.nuevoUsuario);
 
-  router.get("/getusuarios", async (req, res) => {
+  router.get("/getusers", async (req, res) => {
     try {
       const Posts = await Post.find();
       res.json(Posts);
+    } catch (error) {
+      res.json({ message: error });
+    }
+  });
+
+  router.get("/:postId", async (req, res) => {
+    try {
+      const Posts = await Post.findById(req.params.postId);
+      res.json(Posts);
+    } catch (error) {
+      res.json({ message: error });
+    }
+  });
+
+  router.delete("/deleteuser", async (req, res) => {
+    try {
+      const Posts = await Post.deleteMany();
     } catch (error) {
       res.json({ message: error });
     }
