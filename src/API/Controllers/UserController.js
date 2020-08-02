e6de1372f3e1ec8440a215498c8ed2ed464ceab9
FJ-Riveros <fran.etnies@gmail.com>
2020-08-02 13:54:26 +0200

    Created a single file "Controlers" to simplify the calls to the DB

--- src/API/Controllers/UserController.js
+++ src/API/Controllers/UserController.js
@@ -1,15 +1,53 @@
-const pacientes = require("../Models/User.js");
-const listaUsuarios = require("../")
-//Cuando se crea un nuevo cliente
-exports.nuevoUsuario = async (req, res, next) => {
-  //Crear obeto de usuario
-  const usuario = new pacientes(req.body);
+const userModel = require("../Models/User.js");
+const listaUsuarios = require("../");
 
+exports.addUser = async (req, res, next) => {
+  const newUser = new userModel(req.body);
   try {
-    await usuario.save();
+    await newUser.save();
     res.json({ mensaje: "El usuario se agregó correctamente" });
   } catch (error) {
     console.log(error);
     next();
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const userList = await userModel.find();
+    res.json(userList);
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+exports.getSingleUser = async (req, res) => {
+  try {
+    const getSingleUser = await userModel.findById(req.params.userId);
+    res.json(getSingleUser);
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+exports.deleteSingleUser = async (req, res) => {
+  try {
+    const deleteUSer = await userModel.findByIdAndDelete(req.params.userId);
+    res.json({
+      message: `The user ${req.params.userId} was succesfuly deleted `,
+    });
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+exports.updateSingleUser = async (req, res) => {
+  try {
+    const updatedUser = await userModel.findByIdAndUpdate(req.params.userId, {
+      $set: { nombre: req.body.nombre },
+    });
+    res.json({ updatedUser });
+  } catch (error) {
+    res.json({ message: error });
   }
 };
--- src/API/Routes/index.js
+++ src/API/Routes/index.js
@@ -1,50 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const usuarioController = require("../Controllers/UserController");
-const Model = require("../Models/User");
+const userController = require("../Controllers/UserController");
 
 module.exports = function () {
-  router.post("/adduser", usuarioController.nuevoUsuario);
+  router.post("/adduser", userController.addUser);
 
-  router.get("/getusers", async (req, res) => {
-    try {
-      const userList = await Model.find();
-      res.json(userList);
-    } catch (error) {
-      res.json({ message: error });
-    }
-  });
+  router.get("/getusers", userController.getUsers);
 
-  router.get("/searchuser:userId", async (req, res) => {
-    try {
-      const getSingleUser = await Model.findById(req.params.userId);
-      res.json(getSingleUser);
-    } catch (error) {
-      res.json({ message: error });
-    }
-  });
+  router.get("/getuser:userId", userController.getSingleUser);
 
-  router.delete("/deleteuser:userId", async (req, res) => {
-    try {
-      const deleteUSer = await Model.findByIdAndDelete(req.params.userId);
-      res.json({
-        message: `The user ${req.params.userId} was succesfuly deleted `,
-      });
-    } catch (error) {
-      res.json({ message: error });
-    }
-  });
+  router.delete("/deleteuser:userId", userController.deleteSingleUser);
 
-  router.patch("/updateuser:userId", async (req, res) => {
-    try {
-      const updatedUser = await Model.findByIdAndUpdate(req.params.userId, {
-        $set: { nombre: req.body.nombre },
-      });
-      res.json({ updatedUser });
-    } catch (error) {
-      res.json({ message: error });
-    }
-  });
+  router.patch("/updateuser:userId", userController.updateSingleUser);
 
   return router;
 };
