fc7223ea3a6c3554d09ec64b50703dbb29d7aefe
FJ-Riveros <fran.etnies@gmail.com>
2020-08-05 19:08:31 +0200

    Properly getting the user info when registering

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,22 +1,33 @@
 import React from "react";
 
 const Register = () => {
-  const createUser = (event) => {
-    console.log(event.target);
+  const createUser = (e) => {
+    e.preventDefault();
+    const email = new FormData(e.target).get("email");
+    const userName = new FormData(e.target).get("username");
+    const age = new FormData(e.target).get("age");
   };
 
   return (
     <>
       <h1>Register</h1>
       <form onSubmit={(event) => createUser(event)}>
-        <input placeholder="Enter your username" />
+        <input
+          placeholder="Enter your username"
+          name="username"
+          required="required"
+        />
         <input
           type="email"
           name="email"
           required="required"
           placeholder="Enter your email"
         />
-        <input placeholder="Enter your age"></input>
+        <input
+          placeholder="Enter your age"
+          name="age"
+          required="required"
+        ></input>
         <button type="submit">Send</button>
       </form>
     </>
