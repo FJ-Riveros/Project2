7f415091e04ff411e0aa1daee17055318092f60d
FJ-Riveros <fran.etnies@gmail.com>
2020-08-05 19:53:50 +0200

    Adding the fetch post to create a new user

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [formdata, setFormData] = useState({
+    username: "",
+    email: "",
+    age: "",
+  });
   const createUser = (e) => {
-    e.preventDefault();
-    const email = new FormData(e.target).get("email");
-    const userName = new FormData(e.target).get("username");
-    const age = new FormData(e.target).get("age");
+    setFormData(...formdata, ([e.target.name] = e.target.value));
   };
 
+  const submitNewUser = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
       <h1>Register</h1>
-      <form onSubmit={(event) => createUser(event)}>
+      <form onSubmit={(event) => submitNewUser(event)}>
         <input
           placeholder="Enter your username"
           name="username"
           required="required"
+          onChange={createUser}
         />
         <input
           type="email"
           name="email"
           required="required"
           placeholder="Enter your email"
+          onChange={createUser}
         />
         <input
           placeholder="Enter your age"
           name="age"
           required="required"
+          onChange={createUser}
         ></input>
         <button type="submit">Send</button>
       </form>
