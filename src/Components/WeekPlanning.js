627d7d681a453e49b613f46c2428acfadb2e58ec
FJ-Riveros <fran.etnies@gmail.com>
2020-08-27 17:23:44 +0200

    Adding changes to the WeekPlanning component

--- src/Components/WeekPlanning.js
+++ src/Components/WeekPlanning.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import StyledSectionContainer from "./Styled/StyledSectionContainer";
 import { HomeButton } from "../Utils";
 import StyledGrid from "./Styled/StyledWeekPlanning";
 import MediaQuery from "react-responsive";
+import VerticalTab from "./VerticalTab";
 
 const WeekPlanning = () => {
-  // const isTabletOrMobile = useMediaQuery({ query: "(max-width: 799px" });
-  const [isMobile, setIsMobile] = useState(true);
-  const handleMediaQueryChange = (matches) => {
-    console.log(matches);
-    //   matches ? setIsMobile(true) : setIsMobile(false);
-  };
   return (
     <>
       <StyledSectionContainer minheight>
-        <MediaQuery minDeviceWidth={800} onChange={handleMediaQueryChange}>
+        <MediaQuery minDeviceWidth={800}>
           <h2>Week Planning</h2>
-          {isMobile ? console.log("Si es movil") : console.log("No lo es")}
           <StyledGrid>
             <div className="grid-item-colum-header"></div>
             <div className="grid-item-colum-header">Monday</div>
@@ -59,8 +53,12 @@ const WeekPlanning = () => {
             <div className="grid-item-row">1</div>
             <div className="grid-item-row">1</div>
           </StyledGrid>
-          <button>Random week</button>
         </MediaQuery>
+        <MediaQuery maxDeviceWidth={799}>
+          <h2>Week Planning</h2>
+          <VerticalTab></VerticalTab>
+        </MediaQuery>
+        <button>Random week</button>
       </StyledSectionContainer>
       <HomeButton />
     </>
